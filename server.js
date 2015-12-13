e5582f156815654c3e5ec2ceff59dea5b9f3a952
Victor Tang <victortang1992@gmail.com>
2015-12-12 17:22:52 -0800

    express4.0 update

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var flash    = require('connect-flash');
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
+var methodOverride = require('method-override');
 var session      = require('express-session');
 
 var configDB = require('./config/database.js');
@@ -22,9 +23,12 @@ mongoose.connect(configDB.url); // connect to our database
 // require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
+app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: false }))    // parse application/x-www-form-urlencoded
+app.use(bodyParser.json())    // parse application/json
+app.use(methodOverride());                  // simulate DELETE and PUT
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
